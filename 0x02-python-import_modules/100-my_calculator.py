fcfca772c8abbeb106f172d64f8009bc0eb958b7
Frank Anin <frankanin99@gmail.com>
2024-01-05 21:40:30 +0000

    imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
+    if sys.argv[2] == '+':
+        solution = add(a, b)
+    elif sys.argv[2] == '-':
+        solution = sub(a, b)
+    elif sys.argv[2] == '*':
+        solution = mul(a, b)
+    elif sys.argv[2] == '/':
+        solution = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    print(f"{a} {sys.argv[2]} {b} = {solution}")
