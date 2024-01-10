610f766a099c8692e3fdc0ae23fe925c684e53c5
Frank Anin <frankanin99@gmail.com>
2024-01-10 18:29:00 +0000

    More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,11 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_dict = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500,
+            'M': 1000
+            }
     solution = 0
     prev_val = 0
     for c in roman_string:
