95903fdb80108fe82bc04fe13fd9f62bc67946ba
Frank Anin <frankanin99@gmail.com>
2024-01-22 16:57:02 +0000

    raises a type exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 def raise_exception():
+    raise TypeError("Exception raised")
+
+if __name__ == "__main__":
     try:
-        raise TypeError("Exception raised")
+        raise_exception()
     except TypeError as te:
         print(te)
