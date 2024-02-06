3d601c74e53823b342d19c7c3f6fb86f0be7077d
Frank Anin <frankanin99@gmail.com>
2024-02-06 22:13:58 +0000

    Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class MyInt that inherits from int."""
+
+
+class MyInt(int):
+    """Swap int operators == and !="""
+
+    def __eq__(self, other):
+        """Overrides the equality operator (==)"""
+        return (super().__ne__(other))
+
+    def __ne__(self, other):
+        """Overrides the inequality operator (!=)"""
+        return (super().__eq__(other))
