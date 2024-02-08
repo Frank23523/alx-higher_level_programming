a67b154a8e837b6a4fec85757032e6639423a100
Frank Anin <frankanin99@gmail.com>
2024-02-08 00:41:24 +0000

    Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,24 @@
 import sys
 import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
+def save_to_json_file(my_obj, filename):
+    """Write a given object to a text file using its JSON representation"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
+
+
+def load_from_json_file(filename):
+    """
+    Reads a JSON file and returns the Python data structure
+    Returns:
+        object: The Python data structure represented by the JSON content
+    """
+    with open(filename) as file:
+        return json.load(file)
+
+
+if __name__ == "__main__":
     try:
         existing_data = load_from_json_file("add_item.json")
     except FileNotFoundError:
