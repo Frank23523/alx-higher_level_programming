7442034dee613b3984c81e70f3e002c1d9ab338a
Frank Anin <frankanin99@gmail.com>
2024-02-16 16:56:24 +0000

    Writes the JSON string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,19 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_objs (list): list of instances who inherits of Base
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
