4eb5a3ff541ae77791dc8345d9548a07c75ec4ae
Frank Anin <frankanin99@gmail.com>
2024-02-16 17:03:02 +0000

    returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -47,3 +47,17 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns:
+          the list of the JSON string representation json_string.
+          else: an empty list
+
+        Args:
+            json_string (str): string representing a list of dictionaries.
+        """
+        if json_string is None or json_string == "[]":
+            return ([])
+        return (json.loads(json_string))
