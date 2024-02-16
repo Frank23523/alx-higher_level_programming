35fa81967109debafeb6f850b11cf9bc209424a6
Frank Anin <frankanin99@gmail.com>
2024-02-16 17:18:48 +0000

    returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -77,3 +77,14 @@ class Base:
                 dummy = cls(1)
             dummy.update(**dictionary)
             return (dummy)
+
+    @classmethod
+    def load_from_file(cls):
+        """ Return a list of instances """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                list_dicts = cls.from_json_string(file.read())
+                return ([cls.create(**d) for d in list_dicts])
+        except IOError:
+            return ([])
