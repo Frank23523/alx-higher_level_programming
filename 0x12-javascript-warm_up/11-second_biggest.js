9e80d989730907cf343a90a51362b584e55de5f3
Frank Anin <frankanin99@gmail.com>
2024-04-08 23:12:37 +0000

    Javascript - warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args.length <= 1) {
-	console.log(0);
+  console.log(0);
 } else {
-	const integers = args.map(Number).sort((a, b) => b - a);
-	console.log(integers[1]);
+  const integers = args.map(Number).sort((a, b) => b - a);
+  console.log(integers[1]);
 }
