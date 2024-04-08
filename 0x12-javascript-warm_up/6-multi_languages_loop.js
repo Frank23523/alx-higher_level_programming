06d295a1e670565b19cb96422171465d0f8a7df2
Frank Anin <frankanin99@gmail.com>
2024-04-08 23:08:35 +0000

    Javascript - warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const text of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-	console.log(text);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
