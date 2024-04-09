62e6bc5b6465b178aef97a1353e1d4f5fe7e0e28
Frank Anin <frankanin99@gmail.com>
2024-04-09 23:56:41 +0000

    avaScript - Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let rev_list = []
+  for (let len = list.length - 1; len >=0; len--) {
+    rev_list.push(list[len]);
+  }
+  return rev_list;
+};
