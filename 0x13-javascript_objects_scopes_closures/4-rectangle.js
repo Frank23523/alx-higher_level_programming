8db5c169eb7fabf65373a0632594195617a03290
Frank Anin <frankanin99@gmail.com>
2024-04-10 00:04:05 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,11 +13,11 @@ class Rectangle {
     }
   }
 
-  rotate () {
+  rotate() {
     [this.width, this.height] = [this.height, this.width]
   }
 
-  double () {
+  double() {
     this.width *= 2;
     this.height *= 2;
   }
