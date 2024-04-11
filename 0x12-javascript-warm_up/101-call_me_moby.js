ed093bba8cc4fb48b6ed9558ff656830a9b390d1
Frank Anin <frankanin99@gmail.com>
2024-04-11 21:30:34 +0000

    Javascript - Warm up

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function callMeMoby(x, theFunction) {
+function callMeMoby (x, theFunction) {
   if (x > 0) {
     theFunction();
-	callMeMoby(x - 1, theFunction);
+    callMeMoby(x - 1, theFunction);
   }
 }
 
