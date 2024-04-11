455f0809f164d40fd1dafa0157f49932bf354aeb
Frank Anin <frankanin99@gmail.com>
2024-04-11 22:01:27 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const rev_list = [];
+  const revList = [];
   for (let len = list.length - 1; len >= 0; len--) {
-    rev_list.push(list[len]);
+    revList.push(list[len]);
   }
-  return rev_list;
+  return revList;
 };
