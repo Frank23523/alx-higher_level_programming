300795d9c5a0e135cb6d65aa7cf91afc74e0c965
Frank Anin <frankanin99@gmail.com>
2024-04-19 11:45:35 +0000

    lists all states from the database hbtn_0e_0_us

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,25 +7,32 @@ import sys
 
 if __name__ == "__main__":
     # 3 arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # connect to a MySQL server running on localhost at port 3306
-    nection = MySQLdb.connect(host="localhost", port=3306,
-                              user=username, passwd=password, db=db_name)
+    try:
+        # connect to a MySQL server running on localhost at port 3306
+        nection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=db_name,
+            charset="utf8"
+        )
+    except MySQLdb.Error as db_err:
+        print("Error connecting to database: {}".format(db_err))
+        sys.exit(1)
 
     cursor = nection.cursor()
-
     # sorted in ascending order by states.id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
     rows = cursor.fetchall()
 
-    # Display results
+	# Display results
     for row in rows:
         print(row)
 
-    # Close cursor and connection
     cursor.close()
     nection.close()
