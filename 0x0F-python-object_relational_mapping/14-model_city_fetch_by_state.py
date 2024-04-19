e5b47b2ecd39878c86c181a01bde685be31a31a7
Frank Anin <frankanin99@gmail.com>
2024-04-19 17:18:47 +0000

    prints all city objects

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script prints all City objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """engine to get states and 3 arguments"""
+
+    engine = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}"
+            .format(sys.argv[1], sys.argv[2], sys.argv[3])
+            )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    cities = session.query(City, State).join(State)
+
+    for city, state in cities.all:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.commit()
+    session.close()
