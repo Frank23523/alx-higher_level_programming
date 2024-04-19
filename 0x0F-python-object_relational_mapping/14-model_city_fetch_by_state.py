f28754df558737022c60e2ef150eb0464fe7f751
Frank Anin <frankanin99@gmail.com>
2024-04-19 17:36:17 +0000

    prints all city objects

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -20,10 +20,10 @@ if __name__ == "__main__":
 
     session = Session()
 
-    cities = session.query(City).order_by(City.id).all()
+    cities = session.query(City, State).order_by(City.id).all()
 
-    for city in cities:
-        print("{}: ({}) {}".format.(city.state.name, city.id, city.name))
+    for city, state in cities:
+        print("{}: ({}) {}".format.(state.name, city.id, city.name))
 
     session.commit()
     session.close()
