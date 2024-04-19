6bda2be4e7aae01434a839737a2a7d5110deeac3
Frank Anin <frankanin99@gmail.com>
2024-04-19 11:58:42 +0000

    lists all states from the database hbtn_0e_0_us

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,7 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 import MySQLdb
+import sys
 
 
 def list_states(username, password, database):
@@ -34,3 +35,13 @@ def list_states(username, password, database):
 
     # Close the database connection
     db.close()
+
+
+# Example
+if __name__ == '__main__':
+    # 3 arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
