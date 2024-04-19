cdc5cdfb99894cc771b457d825f88ae5f57c614f
Frank Anin <frankanin99@gmail.com>
2024-04-19 14:47:13 +0000

    lists all cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -21,11 +21,9 @@ if __name__ == '__main__':
     with nection.cursor() as cursor:
         cursor.execute("SELECT cities.id, cities.name, states.name \
                 FROM cities \
-                JOIN states ON cities.state_id = states.id \
-                ORDER BY cities.id ASC"
-                )
+                INNER JOIN states ON cities.state_id = states.id \
+                ORDER BY cities.id ASC")
     rows = cursor.fetchall()
 
-    if rows:
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
