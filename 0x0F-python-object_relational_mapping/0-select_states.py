188657617963f6afea976e0692719875c071a536
Frank Anin <frankanin99@gmail.com>
2024-04-19 11:56:29 +0000

    lists all states from the database hbtn_0e_0_us

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,36 +3,34 @@
 This script lists all states from the database hbtn_0e_0_usa.
 """
 import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    # 3 arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
 
-    try:
-        # connect to a MySQL server running on localhost at port 3306
-        nection = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=db_name,
-            charset="utf8"
+def list_states(username, password, database):
+    """lists all states from the database hbtn_0e_0_usa.
+    Ags:
+        username: mysql username
+        password: mysql password
+        database: mysql database
+    """
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
         )
-    except MySQLdb.Error as db_err:
-        print("Error connecting to database: {}".format(db_err))
-        sys.exit(1)
+    cursor = db.cursor()
 
-    cursor = nection.cursor()
-    # sorted in ascending order by states.id
+    # Execute the SQL query to fetch all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows from the query result
     rows = cursor.fetchall()
 
-	# Display results
+    # Display the results
     for row in rows:
         print(row)
 
-    cursor.close()
-    nection.close()
+    # Close the database connection
+    db.close()
