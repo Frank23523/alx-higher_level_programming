380e250c1d808779c238f110d4975ca969f7f2b9
Frank Anin <frankanin99@gmail.com>
2024-04-19 23:48:02 +0000

    create state with city

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     session = Session()
 
     new_state = State(name='California')
-    new_city = City(name='San Francisco')
-    new_state.cities.append(new_city)
-
+    new_city = City(name='San Francisco', state=new_state)
     session.add(new_state)
+    session.add(new_city)
     session.commit()
+
     session.close()
