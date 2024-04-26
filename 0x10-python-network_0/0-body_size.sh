4b1b278537f67f047e6432245e0c4b9603cf4b26
Frank Anin <frankanin99@gmail.com>
2024-04-26 21:48:26 +0000

    Python network 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-# -s to disable the progression display (silent)
-# -i for grep case-sensitive
-# awk to print second column of each line output
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
