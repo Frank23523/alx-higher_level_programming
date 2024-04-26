e9d6313dd4a525823031d0e8c9ad08eb3ebb6f34
Frank Anin <frankanin99@gmail.com>
2024-04-26 22:51:34 +0000

    Python network 0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,18 +3,18 @@
 
 
 def find_peak(list_of_integers):
-	"""finds a peak in a list of unsorted integers"""
-	if list_of_integers == None:
-		return None
-	
-	left = 0
-	right = len(list_of_integers) - 1
+    """finds a peak in a list of unsorted integers"""
+    if list_of_integers is None:
+        return None
 
-	while left < right:
-		middle = (left + right) // 2
-		if list_of_integers[middle] < list_of_integers[middle + 1]:
-			left = middle + 1
-		else:
-			right = middle
-	
-	return list_of_integers[left]
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        middle = (left + right) // 2
+        if list_of_integers[middle] < list_of_integers[middle + 1]:
+            left = middle + 1
+        else:
+            right = middle
+
+    return list_of_integers[left]
