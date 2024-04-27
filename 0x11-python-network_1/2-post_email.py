77d6b5767d82c129ab9ac4fcfdb2265266cbbb4a
Frank Anin <frankanin99@gmail.com>
2024-04-27 01:20:58 +0000

    Python network 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,18 +4,19 @@ import urllib.request
 import urllib.parse
 from sys import argv
 
-url = argv[1]
-email = argv[2]
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
-
-r = urllib.request.Request(url, data=data, method='POST')
 
 if __name__ == "__main__":
+    """
+    takes in a URL and an email, sends a POST request to the passed URL
+    with the email as a parameter, and displays
+    the body of the response (decoded in utf-8)
+    """
+    url = argv[1]
+    email = argv[2]
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    r = urllib.request.Request(url, data=data, method='POST')
+
     with urllib.request.urlopen(r) as response:
-        """
-        takes in a URL and an email, sends a POST request to the passed URL
-        with the email as a parameter, and displays
-        the body of the response (decoded in utf-8)
-        """
         print(response.read().decode('utf-8'))
